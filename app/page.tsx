9df01cb4bfb98c2970cb654daaed2b230373d49f
Tobiiadee <oluwatobi121@gmail.com>
2024-08-09 13:37:12 +0100

    "Fixing ENOENT error"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 /** @format */
 
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className='flex min-h-screen text-2xl min-w-screen items-center justify-center p-24'>
